00290d13bedf4810bc2bcd89a23c90393b754aaa
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-09-11 02:43:46 -0400

    Update assemble.bat

--- assemble.bat
+++ assemble.bat
@@ -1,4 +1,5 @@
 @echo off
+if not exist OUT mkdir OUT
 "TOOL/vasmm68k_psi-x.exe" -altlocal -m68000 -maxerrors=0 -Fbin -start=0 -o "OUT/MEGADRIVE.BIN" -L "OUT/MEGADRIVE.LST" -Lall "SRC/MAIN.ASM" 2> _errors.log
 type _errors.log
 del _errors.log
