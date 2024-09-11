a160eb0c783fb279a40bfa82c637fdebebb652ff
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-09-11 02:42:12 -0400

    structurize

--- assemble.bat
+++ assemble.bat
@@ -1,3 +1,5 @@
-vasmm68k_psi-x.exe -altlocal -m68000 -maxerrors=0 -Fbin -start=0 -o MEGADRIVE.BIN -L MEGADRIVE.LST -Lall MAIN.ASM 2> _errors.log
+@echo off
+"TOOL/vasmm68k_psi-x.exe" -altlocal -m68000 -maxerrors=0 -Fbin -start=0 -o "OUT/MEGADRIVE.BIN" -L "OUT/MEGADRIVE.LST" -Lall "SRC/MAIN.ASM" 2> _errors.log
 type _errors.log
+del _errors.log
 pause
