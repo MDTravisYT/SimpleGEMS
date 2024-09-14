e673085529cb6d143931e349962831c9828ab12e
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-09-14 06:29:17 -0400

    some organization

--- maketst/SRC/main.s
+++ maketst/SRC/main.s
@@ -1,10 +1,10 @@
-	include	macros.i
-	include	equates.i
+	include	SRC/macros.i
+	include	SRC/equates.i
 	
 	section test
 
 init:
 	dc.b	"hi this is a test and shouldnt run"
 	
-	include	globals.i
+	include	SRC/globals.i
 	end
---
+++ maketst/SRC/vectors.s
@@ -0,0 +1,12 @@
+	include	SRC/macros.i
+	include	SRC/equates.i
+
+	section	test
+
+	dc.l	init
+	dc.l	init
+	dc.l	init
+	dc.l	init
+	
+	include	SRC/globals.i
+	end
