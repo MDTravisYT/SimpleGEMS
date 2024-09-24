664754a41efb26ba006e569a3a1f7bacb5e06116
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2024-09-24 16:40:25 -0400

    remove autoplay song on start

--- GEMS/pbank.asm
+++ GEMS/pbank.asm
@@ -2,11 +2,481 @@
 ; Patch Offset Table
 ;
 
-	dc.b	$02,$00		; Offset to patch #0: 2
+	dc.b	$00,$01		; Offset to patch #0: 256
+	dc.b	$27,$01		; Offset to patch #1: 295
+	dc.b	$4E,$01		; Offset to patch #2: 334
+	dc.b	$4F,$01		; Offset to patch #3: 335
+	dc.b	$50,$01		; Offset to patch #4: 336
+	dc.b	$51,$01		; Offset to patch #5: 337
+	dc.b	$52,$01		; Offset to patch #6: 338
+	dc.b	$53,$01		; Offset to patch #7: 339
+	dc.b	$54,$01		; Offset to patch #8: 340
+	dc.b	$55,$01		; Offset to patch #9: 341
+	dc.b	$56,$01		; Offset to patch #10: 342
+	dc.b	$57,$01		; Offset to patch #11: 343
+	dc.b	$58,$01		; Offset to patch #12: 344
+	dc.b	$59,$01		; Offset to patch #13: 345
+	dc.b	$5A,$01		; Offset to patch #14: 346
+	dc.b	$5B,$01		; Offset to patch #15: 347
+	dc.b	$5C,$01		; Offset to patch #16: 348
+	dc.b	$5D,$01		; Offset to patch #17: 349
+	dc.b	$5E,$01		; Offset to patch #18: 350
+	dc.b	$5F,$01		; Offset to patch #19: 351
+	dc.b	$86,$01		; Offset to patch #20: 390
+	dc.b	$87,$01		; Offset to patch #21: 391
+	dc.b	$88,$01		; Offset to patch #22: 392
+	dc.b	$89,$01		; Offset to patch #23: 393
+	dc.b	$8A,$01		; Offset to patch #24: 394
+	dc.b	$8B,$01		; Offset to patch #25: 395
+	dc.b	$8C,$01		; Offset to patch #26: 396
+	dc.b	$8D,$01		; Offset to patch #27: 397
+	dc.b	$8E,$01		; Offset to patch #28: 398
+	dc.b	$8F,$01		; Offset to patch #29: 399
+	dc.b	$90,$01		; Offset to patch #30: 400
+	dc.b	$91,$01		; Offset to patch #31: 401
+	dc.b	$92,$01		; Offset to patch #32: 402
+	dc.b	$B9,$01		; Offset to patch #33: 441
+	dc.b	$BA,$01		; Offset to patch #34: 442
+	dc.b	$BB,$01		; Offset to patch #35: 443
+	dc.b	$BC,$01		; Offset to patch #36: 444
+	dc.b	$BD,$01		; Offset to patch #37: 445
+	dc.b	$BE,$01		; Offset to patch #38: 446
+	dc.b	$BF,$01		; Offset to patch #39: 447
+	dc.b	$C0,$01		; Offset to patch #40: 448
+	dc.b	$C1,$01		; Offset to patch #41: 449
+	dc.b	$C2,$01		; Offset to patch #42: 450
+	dc.b	$C3,$01		; Offset to patch #43: 451
+	dc.b	$C4,$01		; Offset to patch #44: 452
+	dc.b	$C5,$01		; Offset to patch #45: 453
+	dc.b	$C6,$01		; Offset to patch #46: 454
+	dc.b	$C7,$01		; Offset to patch #47: 455
+	dc.b	$C8,$01		; Offset to patch #48: 456
+	dc.b	$EF,$01		; Offset to patch #49: 495
+	dc.b	$16,$02		; Offset to patch #50: 534
+	dc.b	$17,$02		; Offset to patch #51: 535
+	dc.b	$18,$02		; Offset to patch #52: 536
+	dc.b	$19,$02		; Offset to patch #53: 537
+	dc.b	$1A,$02		; Offset to patch #54: 538
+	dc.b	$1B,$02		; Offset to patch #55: 539
+	dc.b	$1C,$02		; Offset to patch #56: 540
+	dc.b	$43,$02		; Offset to patch #57: 579
+	dc.b	$6A,$02		; Offset to patch #58: 618
+	dc.b	$6B,$02		; Offset to patch #59: 619
+	dc.b	$6C,$02		; Offset to patch #60: 620
+	dc.b	$93,$02		; Offset to patch #61: 659
+	dc.b	$94,$02		; Offset to patch #62: 660
+	dc.b	$95,$02		; Offset to patch #63: 661
+	dc.b	$96,$02		; Offset to patch #64: 662
+	dc.b	$97,$02		; Offset to patch #65: 663
+	dc.b	$98,$02		; Offset to patch #66: 664
+	dc.b	$BF,$02		; Offset to patch #67: 703
+	dc.b	$C0,$02		; Offset to patch #68: 704
+	dc.b	$E7,$02		; Offset to patch #69: 743
+	dc.b	$E8,$02		; Offset to patch #70: 744
+	dc.b	$0F,$03		; Offset to patch #71: 783
+	dc.b	$36,$03		; Offset to patch #72: 822
+	dc.b	$37,$03		; Offset to patch #73: 823
+	dc.b	$5E,$03		; Offset to patch #74: 862
+	dc.b	$5F,$03		; Offset to patch #75: 863
+	dc.b	$60,$03		; Offset to patch #76: 864
+	dc.b	$61,$03		; Offset to patch #77: 865
+	dc.b	$62,$03		; Offset to patch #78: 866
+	dc.b	$63,$03		; Offset to patch #79: 867
+	dc.b	$64,$03		; Offset to patch #80: 868
+	dc.b	$65,$03		; Offset to patch #81: 869
+	dc.b	$66,$03		; Offset to patch #82: 870
+	dc.b	$67,$03		; Offset to patch #83: 871
+	dc.b	$68,$03		; Offset to patch #84: 872
+	dc.b	$69,$03		; Offset to patch #85: 873
+	dc.b	$6A,$03		; Offset to patch #86: 874
+	dc.b	$6B,$03		; Offset to patch #87: 875
+	dc.b	$6C,$03		; Offset to patch #88: 876
+	dc.b	$6D,$03		; Offset to patch #89: 877
+	dc.b	$6E,$03		; Offset to patch #90: 878
+	dc.b	$6F,$03		; Offset to patch #91: 879
+	dc.b	$70,$03		; Offset to patch #92: 880
+	dc.b	$71,$03		; Offset to patch #93: 881
+	dc.b	$72,$03		; Offset to patch #94: 882
+	dc.b	$73,$03		; Offset to patch #95: 883
+	dc.b	$74,$03		; Offset to patch #96: 884
+	dc.b	$75,$03		; Offset to patch #97: 885
+	dc.b	$76,$03		; Offset to patch #98: 886
+	dc.b	$77,$03		; Offset to patch #99: 887
+	dc.b	$78,$03		; Offset to patch #100: 888
+	dc.b	$79,$03		; Offset to patch #101: 889
+	dc.b	$7A,$03		; Offset to patch #102: 890
+	dc.b	$7B,$03		; Offset to patch #103: 891
+	dc.b	$7C,$03		; Offset to patch #104: 892
+	dc.b	$7D,$03		; Offset to patch #105: 893
+	dc.b	$7E,$03		; Offset to patch #106: 894
+	dc.b	$7F,$03		; Offset to patch #107: 895
+	dc.b	$80,$03		; Offset to patch #108: 896
+	dc.b	$81,$03		; Offset to patch #109: 897
+	dc.b	$82,$03		; Offset to patch #110: 898
+	dc.b	$83,$03		; Offset to patch #111: 899
+	dc.b	$84,$03		; Offset to patch #112: 900
+	dc.b	$85,$03		; Offset to patch #113: 901
+	dc.b	$86,$03		; Offset to patch #114: 902
+	dc.b	$87,$03		; Offset to patch #115: 903
+	dc.b	$88,$03		; Offset to patch #116: 904
+	dc.b	$AF,$03		; Offset to patch #117: 943
+	dc.b	$D6,$03		; Offset to patch #118: 982
+	dc.b	$FD,$03		; Offset to patch #119: 1021
+	dc.b	$FE,$03		; Offset to patch #120: 1022
+	dc.b	$FF,$03		; Offset to patch #121: 1023
+	dc.b	$00,$04		; Offset to patch #122: 1024
+	dc.b	$01,$04		; Offset to patch #123: 1025
+	dc.b	$02,$04		; Offset to patch #124: 1026
+	dc.b	$03,$04		; Offset to patch #125: 1027
+	dc.b	$04,$04		; Offset to patch #126: 1028
+	dc.b	$05,$04		; Offset to patch #127: 1029
 ;
 ; Patch Data
 ;
 
-; Patch 0 "All Samples": DIG
-	dc.b	1
-	dc.b	$04
+; Patch 0 "piano 1": FM
+	dc.b	0
+	dc.b	$00,$00,$24,$00,$01,$0A,$1F,$00
+	dc.b	$12,$20,$01,$0A,$1F,$00,$12,$20
+	dc.b	$01,$00,$1F,$0B,$1F,$F9,$01,$00
+	dc.b	$1F,$0B,$1F,$F9,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 1 "Piano 2": FM
+	dc.b	0
+	dc.b	$00,$00,$3C,$D1,$01,$14,$1C,$0F
+	dc.b	$0A,$A6,$31,$14,$1C,$0F,$0A,$A6
+	dc.b	$01,$03,$1F,$06,$06,$A9,$71,$03
+	dc.b	$1F,$06,$0E,$A6,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 2 "<Unused #2>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 3 "<Unused #3>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 4 "<Unused #4>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 5 "<Unused #5>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 6 "<Unused #6>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 7 "<Unused #7>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 8 "<Unused #8>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 9 "<Unused #9>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 10 "<Unused #10>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 11 "<Unused #11>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 12 "<Unused #12>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 13 "<Unused #13>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 14 "<Unused #14>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 15 "<Unused #15>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 16 "<Unused #16>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 17 "<Unused #17>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 18 "<Unused #18>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 19 "B3B.FM": FM
+	dc.b	0
+	dc.b	$08,$00,$17,$E4,$33,$07,$5F,$8B
+	dc.b	$01,$8B,$33,$0A,$1D,$9D,$00,$3A
+	dc.b	$71,$0C,$1F,$1F,$00,$3A,$71,$0A
+	dc.b	$5B,$9C,$00,$3A,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 20 "<Unused #20>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 21 "<Unused #21>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 22 "<Unused #22>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 23 "<Unused #23>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 24 "<Unused #24>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 25 "<Unused #25>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 26 "<Unused #26>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 27 "<Unused #27>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 28 "<Unused #28>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 29 "<Unused #29>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 30 "<Unused #30>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 31 "<Unused #31>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 32 "bass.fm": FM
+	dc.b	0
+	dc.b	$02,$00,$04,$C0,$30,$16,$1A,$0F
+	dc.b	$09,$21,$70,$16,$1A,$0F,$09,$21
+	dc.b	$30,$00,$1F,$08,$09,$09,$70,$00
+	dc.b	$1F,$08,$09,$09,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 33 "<Unused #33>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 34 "<Unused #34>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 35 "<Unused #35>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 36 "<Unused #36>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 37 "<Unused #37>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 38 "<Unused #38>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 39 "<Unused #39>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 40 "<Unused #40>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 41 "<Unused #41>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 42 "<Unused #42>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 43 "<Unused #43>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 44 "<Unused #44>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 45 "<Unused #45>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 46 "<Unused #46>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 47 "<Unused #47>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 48 "String Section": FM
+	dc.b	0
+	dc.b	$00,$00,$2C,$C2,$21,$0A,$57,$0C
+	dc.b	$00,$00,$31,$08,$55,$0C,$00,$00
+	dc.b	$53,$00,$51,$1F,$00,$05,$03,$00
+	dc.b	$51,$1F,$00,$05,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 49 "synthstr.fm": FM
+	dc.b	0
+	dc.b	$00,$00,$2C,$C2,$21,$0A,$57,$0C
+	dc.b	$00,$00,$31,$08,$55,$0C,$00,$00
+	dc.b	$53,$00,$48,$1F,$00,$05,$03,$00
+	dc.b	$49,$1F,$00,$05,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 50 "<Unused #50>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 51 "<Unused #51>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 52 "<Unused #52>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 53 "<Unused #53>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 54 "<Unused #54>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 55 "<Unused #55>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 56 "Trumpet": FM
+	dc.b	0
+	dc.b	$0B,$00,$02,$C0,$01,$03,$18,$80
+	dc.b	$02,$32,$51,$03,$98,$80,$02,$32
+	dc.b	$07,$00,$17,$00,$02,$4D,$02,$00
+	dc.b	$97,$00,$02,$4D,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0E,$00
+; Patch 57 "Trombone": FM
+	dc.b	0
+	dc.b	$0B,$00,$35,$D1,$21,$0B,$9A,$8C
+	dc.b	$01,$4B,$21,$17,$98,$80,$02,$37
+	dc.b	$31,$17,$9D,$05,$02,$46,$31,$11
+	dc.b	$9C,$00,$02,$48,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 58 "<Unused #58>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 59 "<Unused #59>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 60 "French Horn": FM
+	dc.b	0
+	dc.b	$0B,$00,$2C,$C0,$11,$14,$99,$80
+	dc.b	$00,$07,$01,$14,$8F,$00,$00,$07
+	dc.b	$11,$00,$94,$00,$02,$48,$01,$00
+	dc.b	$94,$00,$02,$48,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 61 "<Unused #61>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 62 "<Unused #62>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 63 "<Unused #63>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 64 "<Unused #64>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 65 "<Unused #65>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 66 "sax.fm": FM
+	dc.b	0
+	dc.b	$08,$00,$22,$D0,$50,$0F,$0E,$00
+	dc.b	$00,$08,$00,$14,$11,$80,$00,$08
+	dc.b	$37,$28,$0E,$00,$00,$20,$30,$00
+	dc.b	$10,$00,$00,$09,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 67 "<Unused #67>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 68 "Oboe": FM
+	dc.b	0
+	dc.b	$08,$00,$14,$C0,$11,$01,$5D,$9D
+	dc.b	$00,$1A,$31,$01,$1D,$9D,$00,$1A
+	dc.b	$01,$00,$10,$1C,$00,$3A,$51,$00
+	dc.b	$50,$9C,$00,$3A,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 69 "<Unused #69>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 70 "bassoon": FM
+	dc.b	0
+	dc.b	$00,$00,$3C,$00,$01,$10,$16,$00
+	dc.b	$00,$00,$01,$07,$16,$00,$00,$00
+	dc.b	$02,$00,$10,$0A,$00,$0E,$02,$00
+	dc.b	$10,$0A,$00,$0E,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 71 "clarinet": FM
+	dc.b	0
+	dc.b	$08,$00,$14,$C0,$32,$0B,$5D,$9D
+	dc.b	$00,$3A,$32,$0B,$1D,$9D,$00,$3A
+	dc.b	$11,$00,$10,$1C,$00,$3A,$51,$00
+	dc.b	$50,$9C,$00,$3A,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0F,$00
+; Patch 72 "<Unused #72>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 73 "Flute": FM
+	dc.b	0
+	dc.b	$0A,$00,$1F,$D0,$01,$01,$0E,$80
+	dc.b	$04,$19,$01,$01,$0E,$80,$04,$19
+	dc.b	$01,$1E,$0E,$80,$05,$29,$01,$03
+	dc.b	$0E,$80,$05,$29,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$03,$00
+; Patch 74 "<Unused #74>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 75 "<Unused #75>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 76 "<Unused #76>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 77 "<Unused #77>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 78 "<Unused #78>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 79 "<Unused #79>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 80 "<Unused #80>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 81 "<Unused #81>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 82 "<Unused #82>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 83 "<Unused #83>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 84 "<Unused #84>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 85 "<Unused #85>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 86 "<Unused #86>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 87 "<Unused #87>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 88 "<Unused #88>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 89 "<Unused #89>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 90 "<Unused #90>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 91 "<Unused #91>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 92 "<Unused #92>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 93 "<Unused #93>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 94 "<Unused #94>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 95 "<Unused #95>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 96 "<Unused #96>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 97 "<Unused #97>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 98 "<Unused #98>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 99 "<Unused #99>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 100 "<Unused #100>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 101 "<Unused #101>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 102 "<Unused #102>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 103 "<Unused #103>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 104 "<Unused #104>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 105 "<Unused #105>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 106 "<Unused #106>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 107 "<Unused #107>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 108 "<Unused #108>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 109 "<Unused #109>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 110 "<Unused #110>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 111 "<Unused #111>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 112 "<Unused #112>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 113 "<Unused #113>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 114 "<Unused #114>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 115 "<Unused #115>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 116 "Wood Drum": FM
+	dc.b	0
+	dc.b	$07,$09,$3B,$C8,$05,$28,$1F,$14
+	dc.b	$03,$85,$01,$19,$1D,$02,$0D,$26
+	dc.b	$01,$1E,$1F,$00,$05,$D4,$00,$00
+	dc.b	$1F,$0D,$0D,$F6,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 117 "kickdrum.fm": FM
+	dc.b	0
+	dc.b	$0A,$00,$22,$C0,$00,$14,$1F,$18
+	dc.b	$09,$0F,$00,$14,$1F,$9F,$0D,$08
+	dc.b	$00,$00,$1F,$15,$1F,$20,$00,$00
+	dc.b	$1F,$13,$1F,$D2,$1F,$FF,$1F,$FF
+	dc.b	$1F,$FF,$1F,$FF,$0D,$00
+; Patch 118 "defsnare.fm": FM
+	dc.b	0
+	dc.b	$00,$00,$34,$C0,$01,$02,$1F,$00
+	dc.b	$08,$00,$00,$04,$1F,$13,$14,$08
+	dc.b	$02,$00,$1F,$0F,$1F,$F8,$01,$00
+	dc.b	$1F,$13,$1F,$FA,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0F,$00
+; Patch 119 "<Unused #119>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 120 "<Unused #120>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 121 "<Unused #121>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 122 "<Unused #122>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 123 "<Unused #123>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 124 "<Unused #124>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 125 "<Unused #125>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 126 "<Unused #126>": Unknown Type!
+	dc.b	99		; unknown/undefined patch type
+; Patch 127 "hihat.fm": FM
+	dc.b	0
+	dc.b	$00,$00,$02,$C0,$0D,$00,$14,$0A
+	dc.b	$1C,$54,$0D,$00,$1D,$13,$1D,$A1
+	dc.b	$0F,$00,$1F,$0D,$15,$66,$0F,$00
+	dc.b	$1D,$13,$1D,$A8,$00,$00,$00,$00
+	dc.b	$00,$00,$00,$00,$0D,$00
